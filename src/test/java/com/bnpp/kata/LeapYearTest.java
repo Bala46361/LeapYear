94f754921d46a71442c28e6323b71bd3174084d9
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 12:29:44 +0530

    Refactored : Extracted instance initialization in test class to make it reusable

--- src/test/java/com/bnpp/kata/LeapYearTest.java
+++ src/test/java/com/bnpp/kata/LeapYearTest.java
@@ -2,22 +2,29 @@ package com.bnpp.kata;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LeapYearTest {
 
+	LeapYear leapYear;
+
+	@Before
+	public void initialize() {
+		leapYear = new LeapYear();
+	}
+
 	@Test
 	public void shouldReturnTrueWhenYear1996IsPassedAsInput() {
-		LeapYear leapYear = new LeapYear();
 		leapYear.setYear(1996);
+
 		assertTrue(leapYear.isLeapYear());
 	}
-	
-	
+
 	@Test
 	public void shouldReturnTrueWhenYear1992IsDivisibleBy4() {
-		LeapYear leapYear = new LeapYear();
 		leapYear.setYear(1992);
+
 		assertTrue(leapYear.isLeapYear());
 	}
 
