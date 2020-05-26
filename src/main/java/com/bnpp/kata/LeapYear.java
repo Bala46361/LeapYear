4b5d980c459d377a213f949be9e8d20aa034ed1e
Balasubramanian <bala_465@rediffmail.com>
2020-05-26 12:40:39 +0530

    Refactored: Extracted methods and constants for better readability

--- src/main/java/com/bnpp/kata/LeapYear.java
+++ src/main/java/com/bnpp/kata/LeapYear.java
@@ -1,13 +1,26 @@
 package com.bnpp.kata;
 
 public class LeapYear {
+	private static final int FOUR_HUNDRED = 400;
 	private static final int HUNDRED = 100;
 	private static final int FOUR = 4;
 	private static final int ZERO = 0;
 	private int year;
 
 	public boolean isLeapYear() {
-		return (year % FOUR == ZERO && year % HUNDRED != ZERO) || year % 400 == 0;
+		return (isYearDivisibleBy4() && isYearNotDivisibleBy100()) || isYearDivisibleBy400();
+	}
+
+	private boolean isYearDivisibleBy400() {
+		return year % FOUR_HUNDRED == ZERO;
+	}
+
+	private boolean isYearNotDivisibleBy100() {
+		return year % HUNDRED != ZERO;
+	}
+
+	private boolean isYearDivisibleBy4() {
+		return year % FOUR == ZERO;
 	}
 
 	public void setYear(int year) {
